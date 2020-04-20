163a1b1212288380b488f0d8cb46cecd2e24ca13
Jun Lee <heavensarch@gmail.com>
2020-04-20 09:52:30 -0400

    Added routes for user

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
+const users = require("./routes/api/users");
+const bobas = require("./routes/api/bobas");
 
 mongoose
-    .connect(db, { useNewUrlParser: true })
-    .then(() => console.log("Connected to MongoDB successfully"))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/bobas", bobas);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));

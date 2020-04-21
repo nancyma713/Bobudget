8226c2fe721de5308d684c150781243a53b2c451
Jun Lee <heavensarch@gmail.com>
2020-04-21 18:56:43 -0400

    Initial push to heroku

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const db = require("./config/keys").mongoURI;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })

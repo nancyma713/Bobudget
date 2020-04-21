461417dc2c13747274060b7d52b31c8723515ade
Jun Lee <heavensarch@gmail.com>
2020-04-21 18:18:04 -0400

    Add floating bubbles to splash

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,11 +2,12 @@ import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch } from "react-router-dom";
 import NavBarContainer from "./nav/navbar_container";
-import '../assets/stylesheets/app.scss';
+import "../assets/stylesheets/app.scss";
 import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
-// import DashboardContainer from "./dashboard/dashboard_container";
+import DashboardContainer from "./dashboard/dashboard_container";
+import Dashboard from "./dashboard/dashboard";
 
 const App = () => (
   <div>
@@ -15,7 +16,7 @@ const App = () => (
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      {/* <AuthRoute exact path="/dashboard" component={DashboardContainer} /> */}
+      <ProtectedRoute exact path="/dashboard" component={Dashboard} />
     </Switch>
   </div>
 );

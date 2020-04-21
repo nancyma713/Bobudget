416f58afb1ef2afcf434bd3098f9e8f04437d85a
Jun Lee <heavensarch@gmail.com>
2020-04-21 10:06:17 -0400

    Close user-auth branch

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,54 +23,57 @@ router.get(
   }
 );
 
-router.post("/register", (req, res) => {
-  const { errors, isValid } = validateRegisterInput(req.body);
+router.post(
+  "/register",
+  (req, res) => {
+    const { errors, isValid } = validateRegisterInput(req.body);
 
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  User.findOne({ username: req.body.username }).then((user) => {
-    if (user) {
-      errors.username = "Username already exists";
+    if (!isValid) {
       return res.status(400).json(errors);
-    } else {
-      const newUser = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username,
-        password: req.body.password,
-        zipcode: req.body.zipcode,
-        budget: req.body.budget,
-      });
-
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then((user) => {
-              const payload = { id: user.id, username: user.username };
-
-              jwt.sign(
-                payload,
-                keys.secretOrKey,
-                { expiresIn: 3600 },
-                (err, token) => {
-                  res.json({
-                    success: true,
-                    token: "Bearer " + token,
-                  });
-                }
-              );
-            })
-            .catch((err) => console.log(err));
-        });
-      });
     }
-  });
-});
+
+    User.findOne({ username: req.body.username }).then((user) => {
+      if (user) {
+        errors.username = "Username already exists";
+        return res.status(400).json(errors);
+      } else {
+        const newUser = new User({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          username: req.body.username,
+          password: req.body.password,
+          zipcode: req.body.zipcode,
+          budget: req.body.budget,
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then((user) => {
+                const payload = { id: user.id, username: user.username };
+
+                jwt.sign(
+                  payload,
+                  keys.secretOrKey,
+                  { expiresIn: 3600 },
+                  (err, token) => {
+                    res.json({
+                      success: true,
+                      token: "Bearer " + token,
+                    });
+                  }
+                );
+              })
+              .catch((err) => console.log(err));
+          });
+        });
+      }
+    });
+  }
+);
 
 router.post("/login", (req, res) => {
   const { errors, isValid } = validateLoginInput(req.body);

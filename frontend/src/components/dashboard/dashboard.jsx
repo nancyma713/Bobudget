7b00cd63ba9e139e451d2f189b9dcf4c0685a35a
nancyma713 <nancyma713@outlook.com>
2020-04-21 22:31:21 -0400

    update splash buttons

--- frontend/src/components/dashboard/dashboard.jsx
+++ frontend/src/components/dashboard/dashboard.jsx
@@ -46,7 +46,7 @@ class Dashboard extends React.Component {
 
           <div className="calculator">
             <div className="calc-screen">
-              hello
+              you are spending too much on boba
             </div>
             <div className="calc-buttons">
               <div className="button" />

3ebaa66e1e29bc93c23c4165009397e189ce723f
nancyma713 <nancyma713@outlook.com>
2020-04-24 14:07:04 -0400

    Add developers page

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -11,8 +11,7 @@ import MainPage from "./main/main_page";
 import LoginFormContainer from "./session/login_form_container";
 import SignupFormContainer from "./session/signup_form_container";
 import DashboardContainer from "./dashboard/dashboard_container";
-// import Dashboard from "./dashboard/dashboard";
-
+import Creators from "./creators/creators";
 import Search from "./search/search_container";
 import PurchaseHistoryContainer from './purchase_history/purchase_history_container';
 
@@ -25,6 +24,7 @@ import MapContainer from "./map/map_container";
 const App = () => (
   <div>
     <NavBarContainer />
+    <Route exact path='/creators' component={Creators} />
     <Switch>
       <AuthRoute exact path="/" component={MainPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
@@ -33,13 +33,10 @@ const App = () => (
       <ProtectedRoute exact path="/dashboard" component={DashboardContainer} />
       <ProtectedRoute exact path="/search" component={Search} />
 
-
-
       {/* testing for map-START */}
         <Route exact path='/map' component={MapContainer}/>
       {/* END */}
       
-
       <ProtectedRoute exact path="/purchases" component={PurchaseHistoryContainer} />
     </Switch>
   </div>

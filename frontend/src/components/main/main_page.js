60855aab3f2262f0bbbcef91a5dc23b6b2e66e56
kara <karaliuu@gmail.com>
2020-05-19 02:55:32 +0800

    Made splash page dynamic and mobile friendly

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -3,18 +3,29 @@ import { Link } from "react-router-dom";
 import "../../assets/stylesheets/splash.scss";
 
 const MainPage = () => (
-  <div className="welcome-form center">
-    <section>
-      <h1>Welcome, 
-        <br />Boba Enthusiasts!&nbsp;&nbsp;
-        <i className="far fa-laugh-wink" /></h1>
-      <h2>Get poppin' below:</h2>
-    </section>
+  // <div id="welcome" className="center">
+  //   <section>
+  //     <h1>Welcome, 
+  //       <br />Boba Enthusiasts!&nbsp;&nbsp;
+  //       <i className="far fa-laugh-wink" /></h1>
+  //     <h2>Get poppin' below:</h2>
+  //   </section>
 
-    <section className="flex-column">
-      <Link to="/signup">Signup</Link>
-      <Link to="/login">Login</Link>
-    </section>
+  //   <section className="flex-column">
+  //     <Link to="/signup">Signup</Link>
+  //     <Link to="/login">Login</Link>
+  //   </section>
+  // </div>
+
+  <div id="welcome-outer">
+    <div id="welcome-middle">
+      <div id="welcome-inner">
+        <h1>Welcome, <br />Boba Enthusiasts&nbsp;&nbsp;<i className="far fa-laugh-wink" /></h1>
+
+        <Link to="/signup">Sign up</Link>
+        <Link to="/login">Log in</Link>
+      </div>
+    </div>
   </div>
 );
 
